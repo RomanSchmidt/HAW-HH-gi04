eeb1579f0866414aac6d40b8a89b38462b381028
Roman Schmdit <roman.schmidt@haw-schmidt.de>
2018-12-19 14:18:28 +0100

    show errors!

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
      * Time: 10:53
      */
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     use Contact\Container\Comments;
     use Contact\Container\Contacts;
     use Contact\Controller;
